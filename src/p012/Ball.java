0f67190e3e14d5f4b40c86b05ac7e1129a028e81
Iván <iic0004@alu.ubu.es>
2017-03-06 20:11:20 +0100

    Código seguro en entorno concurrente

--- src/p012/Ball.java
+++ src/p012/Ball.java
@@ -3,7 +3,6 @@ package p012;
 import java.awt.Image;
 import javax.swing.ImageIcon;
 
-//TODO Transform the code to be used safely in a concurrent context.  
 public class Ball {
 	
 	private String Ball = "Ball.png";
@@ -26,7 +25,7 @@ public class Ball {
 				&& getY() < Board.BOTTOMBOARD);
 	}
 
-	public void move() {
+	public synchronized void move() {
 		v = v * Math.exp(-v / 1000);
 		dx = v * Math.cos(fi);
 		dy = v * Math.sin(fi);
@@ -41,7 +40,7 @@ public class Ball {
 		
 	}
 
-	public void reflect() {
+	public synchronized void reflect() {
 		if (Math.abs(x + 32 - Board.RIGHTBOARD) < Math.abs(dx)) {
 			fi = Math.PI - fi;
 		}
