838a5834d49b4d5adb450523c800806133964fd7
Marta <mmb0093@alu.ubu.es>
2017-03-06 08:53:02 +0100

    Clase HiloBola

---
+++ src/p012/HiloBola.java
@@ -0,0 +1,25 @@
+package p012;
+
+public class HiloBola implements Runnable {
+
+	private final Ball bola;
+
+	public HiloBola(Ball bola) {
+		this.bola = bola;
+	}
+
+	@Override
+	public void run() {
+		while (true) {
+			bola.move();
+			bola.reflect();
+			try {
+				Thread.sleep(100);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+
+	}
+
+}
